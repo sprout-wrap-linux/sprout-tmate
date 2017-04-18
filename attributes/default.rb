f5c7b21c4c9b52b2635d99cea7a55bda0ece2042
Colin Shield <pair+cshield@pivotal.io>
2017-04-18 22:36:11 +0000

    update attributes and remove logging

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 
-default['sprout']['direnv']['version'] = 'v2.11.3'
-default['sprout']['direnv']['url'] = "https://github.com/direnv/direnv/releases/download/#{node['sprout']['direnv']['version']}/direnv.linux-amd64"
+default['sprout']['tmate']['version'] = '2.2.1'
+default['sprout']['tmate']['uri'] = 'https://github.com/tmate-io/tmate/releases/download'
+default['sprout']['tmate']['checksum'] = 'ffb2e75e9f58cb082f6cabb6e0a4794b0e22b037dc82abc3bc7ee8f376f44e23'
